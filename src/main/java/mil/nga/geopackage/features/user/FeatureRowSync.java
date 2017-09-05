5c12d7efe9eea5b97cc4545b3b540154bd9ab29a
Brian Osborn <bosborn@users.noreply.github.com>
2017-09-05 07:06:59 -0600

    user row syncs

---
+++ src/main/java/mil/nga/geopackage/features/user/FeatureRowSync.java
@@ -0,0 +1,22 @@
+package mil.nga.geopackage.features.user;
+
+import mil.nga.geopackage.user.UserRowSync;
+
+/**
+ * Feature Row Sync to support reading a single feature row copy when multiple
+ * near simultaneous asynchronous requests are made
+ *
+ * @author osbornb
+ * @since 1.3.2
+ */
+public class FeatureRowSync extends
+		UserRowSync<FeatureColumn, FeatureTable, FeatureRow> {
+
+	/**
+	 * Constructor
+	 */
+	public FeatureRowSync() {
+
+	}
+
+}
