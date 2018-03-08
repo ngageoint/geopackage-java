1689d70f6171ac8c71dd46b13f683fef91d681f1
Brian Osborn <bosborn@users.noreply.github.com>
2018-03-08 08:25:31 -0700

    coverage data, use the first sorted found value

--- src/main/java/mil/nga/geopackage/extension/coverage/CoverageData.java
+++ src/main/java/mil/nga/geopackage/extension/coverage/CoverageData.java
@@ -776,43 +776,50 @@ public abstract class CoverageData<TImage extends CoverageDataImage> extends
 					for (int y = minDestY; y <= maxDestY; y++) {
 						for (int x = minDestX; x <= maxDestX; x++) {
 
-							// Determine the coverage data based upon the
-							// selected algorithm
-							Double value = null;
-							switch (algorithm) {
-							case NEAREST_NEIGHBOR:
-								value = getNearestNeighborValue(griddedTile,
-										image, leftLastColumns, topLeftRows,
-										topRows, y, x, widthRatio, heightRatio,
-										dest.getTop(), dest.getLeft(),
-										src.getTop(), src.getLeft());
-								break;
-							case BILINEAR:
-								value = getBilinearInterpolationValue(
-										griddedTile, image, leftLastColumns,
-										topLeftRows, topRows, y, x, widthRatio,
-										heightRatio, dest.getTop(),
-										dest.getLeft(), src.getTop(),
-										src.getLeft());
-								break;
-							case BICUBIC:
-								value = getBicubicInterpolationValue(
-										griddedTile, image, leftLastColumns,
-										topLeftRows, topRows, y, x, widthRatio,
-										heightRatio, dest.getTop(),
-										dest.getLeft(), src.getTop(),
-										src.getLeft());
-								break;
-							default:
-								throw new UnsupportedOperationException(
-										"Algorithm is not supported: "
-												+ algorithm);
-							}
+							if (values[y][x] == null) {
 
-							if (value != null) {
-								values[y][x] = value;
-							}
+								// Determine the coverage data based upon the
+								// selected algorithm
+								Double value = null;
+								switch (algorithm) {
+								case NEAREST_NEIGHBOR:
+									value = getNearestNeighborValue(
+											griddedTile, image,
+											leftLastColumns, topLeftRows,
+											topRows, y, x, widthRatio,
+											heightRatio, dest.getTop(),
+											dest.getLeft(), src.getTop(),
+											src.getLeft());
+									break;
+								case BILINEAR:
+									value = getBilinearInterpolationValue(
+											griddedTile, image,
+											leftLastColumns, topLeftRows,
+											topRows, y, x, widthRatio,
+											heightRatio, dest.getTop(),
+											dest.getLeft(), src.getTop(),
+											src.getLeft());
+									break;
+								case BICUBIC:
+									value = getBicubicInterpolationValue(
+											griddedTile, image,
+											leftLastColumns, topLeftRows,
+											topRows, y, x, widthRatio,
+											heightRatio, dest.getTop(),
+											dest.getLeft(), src.getTop(),
+											src.getLeft());
+									break;
+								default:
+									throw new UnsupportedOperationException(
+											"Algorithm is not supported: "
+													+ algorithm);
+								}
 
+								if (value != null) {
+									values[y][x] = value;
+								}
+
+							}
 						}
 					}
 
