92725599b133c75af1c75117d703cd681d00d54b
Brian Osborn <bosborn@users.noreply.github.com>
2018-05-24 14:25:08 -0600

    delete by id methods

--- src/main/java/mil/nga/geopackage/extension/related/UserMappingDao.java
+++ src/main/java/mil/nga/geopackage/extension/related/UserMappingDao.java
@@ -61,4 +61,99 @@ public class UserMappingDao
 		return userMappingDb;
 	}
 
+	/**
+	 * Delete user mappings by base id
+	 * 
+	 * @param userMappingRow
+	 *            user mapping row
+	 * @return rows deleted
+	 */
+	public int deleteByBaseId(UserMappingRow userMappingRow) {
+		return deleteByBaseId(userMappingRow.getBaseId());
+	}
+
+	/**
+	 * Delete user mappings by base id
+	 * 
+	 * @param baseId
+	 *            base id
+	 * @return rows deleted
+	 */
+	public int deleteByBaseId(long baseId) {
+
+		StringBuilder where = new StringBuilder();
+		where.append(buildWhere(UserMappingTable.COLUMN_BASE_ID, baseId));
+
+		String[] whereArgs = buildWhereArgs(new Object[] { baseId });
+
+		int deleted = delete(where.toString(), whereArgs);
+
+		return deleted;
+	}
+
+	/**
+	 * Delete user mappings by related id
+	 * 
+	 * @param userMappingRow
+	 *            user mapping row
+	 * @return rows deleted
+	 */
+	public int deleteByRelatedId(UserMappingRow userMappingRow) {
+		return deleteByRelatedId(userMappingRow.getRelatedId());
+	}
+
+	/**
+	 * Delete user mappings by related id
+	 * 
+	 * @param relatedId
+	 *            related id
+	 * @return rows deleted
+	 */
+	public int deleteByRelatedId(long relatedId) {
+
+		StringBuilder where = new StringBuilder();
+		where.append(buildWhere(UserMappingTable.COLUMN_RELATED_ID, relatedId));
+
+		String[] whereArgs = buildWhereArgs(new Object[] { relatedId });
+
+		int deleted = delete(where.toString(), whereArgs);
+
+		return deleted;
+	}
+
+	/**
+	 * Delete user mappings by both base id and related id
+	 * 
+	 * @param userMappingRow
+	 *            user mapping row
+	 * @return rows deleted
+	 */
+	public int deleteByIds(UserMappingRow userMappingRow) {
+		return deleteByIds(userMappingRow.getBaseId(),
+				userMappingRow.getRelatedId());
+	}
+
+	/**
+	 * Delete user mappings by both base id and related id
+	 * 
+	 * @param baseId
+	 *            base id
+	 * @param relatedId
+	 *            related id
+	 * @return rows deleted
+	 */
+	public int deleteByIds(long baseId, long relatedId) {
+
+		StringBuilder where = new StringBuilder();
+		where.append(buildWhere(UserMappingTable.COLUMN_BASE_ID, baseId));
+		where.append(" AND ");
+		where.append(buildWhere(UserMappingTable.COLUMN_RELATED_ID, relatedId));
+
+		String[] whereArgs = buildWhereArgs(new Object[] { baseId, relatedId });
+
+		int deleted = delete(where.toString(), whereArgs);
+
+		return deleted;
+	}
+
 }
