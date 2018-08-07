a58ae2213aa3476fdffe82fd91ef86fe1210de7f
Brian Osborn <bosborn@users.noreply.github.com>
2018-08-07 13:41:17 -0600

    ignore case when comparing feature table geometry column name to geometry columns column name

--- src/main/java/mil/nga/geopackage/features/user/FeatureTableReader.java
+++ src/main/java/mil/nga/geopackage/features/user/FeatureTableReader.java
@@ -49,7 +49,7 @@ public class FeatureTableReader
 			String name, String type, Long max, boolean notNull,
 			int defaultValueIndex, boolean primaryKey) {
 
-		boolean geometry = name.equals(geometryColumns.getColumnName());
+		boolean geometry = name.equalsIgnoreCase(geometryColumns.getColumnName());
 
 		GeometryType geometryType = null;
 		GeoPackageDataType dataType = null;
