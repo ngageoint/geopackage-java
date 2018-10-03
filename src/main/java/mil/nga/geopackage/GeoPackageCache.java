e14b1402eeffd079138c2cafe375d00f9cccabb2
Brian Osborn <bosborn@users.noreply.github.com>
2018-10-03 09:52:53 -0600

    get or open without caching options

--- src/main/java/mil/nga/geopackage/GeoPackageCache.java
+++ src/main/java/mil/nga/geopackage/GeoPackageCache.java
@@ -40,10 +40,53 @@ public class GeoPackageCache extends GeoPackageCoreCache<GeoPackage> {
 	 * @return GeoPackage
 	 */
 	public GeoPackage getOrOpen(String name, File file) {
+		return getOrOpen(name, file, true);
+	}
+
+	/**
+	 * Get the cached GeoPackage or open the GeoPackage file without caching it
+	 *
+	 * @param file
+	 *            GeoPackage file
+	 * @return GeoPackage
+	 * @since 3.1.0
+	 */
+	public GeoPackage getOrNoCacheOpen(File file) {
+		return getOrNoCacheOpen(file.getName(), file);
+	}
+
+	/**
+	 * Get the cached GeoPackage or open the GeoPackage file without caching it
+	 *
+	 * @param name
+	 *            GeoPackage name
+	 * @param file
+	 *            GeoPackage file
+	 * @return GeoPackage
+	 * @since 3.1.0
+	 */
+	public GeoPackage getOrNoCacheOpen(String name, File file) {
+		return getOrOpen(name, file, false);
+	}
+
+	/**
+	 * Get the cached GeoPackage or open the GeoPackage file without caching it
+	 *
+	 * @param name
+	 *            GeoPackage name
+	 * @param file
+	 *            GeoPackage file
+	 * @param cache
+	 *            true to cache opened GeoPackages
+	 * @return GeoPackage
+	 */
+	private GeoPackage getOrOpen(String name, File file, boolean cache) {
 		GeoPackage geoPackage = get(name);
 		if (geoPackage == null) {
 			geoPackage = GeoPackageManager.open(name, file);
-			add(geoPackage);
+			if (cache) {
+				add(geoPackage);
+			}
 		}
 		return geoPackage;
 	}
