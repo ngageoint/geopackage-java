c57f5da9f697d5cbf18437fd22ab9ce2b3063dd3
Brian Osborn <bosborn@users.noreply.github.com>
2018-10-18 14:28:05 -0600

    contents id tests

---
+++ src/test/java/mil/nga/geopackage/test/extension/contents/ContentsIdCreateTest.java
@@ -0,0 +1,35 @@
+package mil.nga.geopackage.test.extension.contents;
+
+import java.sql.SQLException;
+
+import mil.nga.geopackage.test.CreateGeoPackageTestCase;
+
+import org.junit.Test;
+
+/**
+ * Test Contents Id from a created database
+ * 
+ * @author osbornb
+ */
+public class ContentsIdCreateTest extends CreateGeoPackageTestCase {
+
+	/**
+	 * Constructor
+	 */
+	public ContentsIdCreateTest() {
+
+	}
+
+	/**
+	 * Test contents id
+	 * 
+	 * @throws SQLException
+	 */
+	@Test
+	public void testContentsId() throws SQLException {
+
+		ContentsIdUtils.testContentsId(geoPackage);
+
+	}
+
+}
