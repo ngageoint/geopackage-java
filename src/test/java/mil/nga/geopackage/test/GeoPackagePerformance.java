5f8bed7fb98db7f8a8998f3856c53f4f72002966
Brian Osborn <bosborn@gmail.com>
2020-05-18 12:31:38 -0600

    performance testing updates

--- src/test/java/mil/nga/geopackage/test/GeoPackagePerformance.java
+++ src/test/java/mil/nga/geopackage/test/GeoPackagePerformance.java
@@ -10,6 +10,8 @@ import java.util.logging.Logger;
 import mil.nga.geopackage.BoundingBox;
 import mil.nga.geopackage.GeoPackage;
 import mil.nga.geopackage.db.TableColumnKey;
+import mil.nga.geopackage.db.master.SQLiteMaster;
+import mil.nga.geopackage.db.master.SQLiteMasterType;
 import mil.nga.geopackage.features.columns.GeometryColumns;
 import mil.nga.geopackage.features.user.FeatureDao;
 import mil.nga.geopackage.features.user.FeatureRow;
@@ -40,6 +42,7 @@ public class GeoPackagePerformance {
 	private static final boolean AUTO_COMMIT = false;
 	private static final int COMMIT_CHUNK = 1000;
 	private static final int LOG_CHUNK = 1000000;
+	private static final boolean CHUNK_LOGGING = true;
 
 	/**
 	 * Test feature inserts
@@ -64,7 +67,7 @@ public class GeoPackagePerformance {
 		if (!AUTO_COMMIT) {
 			LOGGER.log(Level.INFO, "Commit Chunk: " + COMMIT_CHUNK);
 		}
-		if (LOG_CHUNK > 0) {
+		if (CHUNK_LOGGING) {
 			LOGGER.log(Level.INFO, "Log Chunk: " + LOG_CHUNK);
 		}
 
@@ -90,6 +93,10 @@ public class GeoPackagePerformance {
 		geoPackage.createFeatureTableWithMetadata(geometryColumns, boundingBox,
 				srs.getId());
 
+		SQLiteMaster table = SQLiteMaster.queryByType(geoPackage.getDatabase(),
+				SQLiteMasterType.TABLE, TABLE_NAME);
+		LOGGER.log(Level.INFO, table.getSql(0));
+
 		GeoPackageGeometryData geometryData = new GeoPackageGeometryData(
 				srs.getSrsId());
 		geometryData.setGeometry(geometry);
@@ -104,41 +111,71 @@ public class GeoPackagePerformance {
 		// Connection connection = dao.getConnection();
 		// connection.setAutoCommit(AUTO_COMMIT);
 
+		int count = 1;
+
+		Duration chunkInsertDuration = null;
+		Instant chunkStartTime = null;
+
+		Duration insertDuration = Duration.ofMillis(0);
+		Instant startTime = Instant.now();
+
+		if (CHUNK_LOGGING) {
+			chunkInsertDuration = insertDuration;
+			chunkStartTime = startTime;
+		}
+
 		try {
 
-			Instant startTime = Instant.now();
-			Instant logTime = Instant.now();
-
-			for (int count = 1; count <= CREATE_COUNT; count++) {
+			for (; count <= CREATE_COUNT; count++) {
 
 				FeatureRow newRow = dao.newRow();
 				newRow.setGeometry(geometryData);
 
+				Instant beforeInsert = Instant.now();
 				dao.create(newRow);
-
-				if (!AUTO_COMMIT && count % COMMIT_CHUNK == 0) {
-					dao.commit();
+				Duration insert = Duration.between(beforeInsert, Instant.now());
+				insertDuration = insertDuration.plus(insert);
+				if (CHUNK_LOGGING) {
+					chunkInsertDuration = chunkInsertDuration.plus(insert);
 				}
 
-				if (LOG_CHUNK > 0 && count % LOG_CHUNK == 0) {
+				if (!AUTO_COMMIT && count % COMMIT_CHUNK == 0) {
+					beforeInsert = Instant.now();
+					dao.commit();
+					insert = Duration.between(beforeInsert, Instant.now());
+					insertDuration = insertDuration.plus(insert);
+					if (CHUNK_LOGGING) {
+						chunkInsertDuration = chunkInsertDuration.plus(insert);
+					}
+				}
+
+				if (CHUNK_LOGGING && count % LOG_CHUNK == 0) {
 					Instant time = Instant.now();
 					LOGGER.log(Level.INFO, "Total Count: " + count);
-					Duration duration = Duration.between(logTime, time);
-					LOGGER.log(Level.INFO, "Chunk Time: "
-							+ duration.toString().substring(2));
+					Duration duration = Duration.between(chunkStartTime, time);
+					LOGGER.log(Level.INFO,
+							"Chunk Time: " + duration.toString().substring(2));
 					LOGGER.log(Level.INFO,
 							"Chunk Average: "
 									+ (duration.toMillis() / (float) LOG_CHUNK)
 									+ " ms");
+					LOGGER.log(Level.INFO,
+							"Chunk Insert Average: "
+									+ (chunkInsertDuration.toMillis()
+											/ (float) LOG_CHUNK)
+									+ " ms");
 					Duration totalDuration = Duration.between(startTime, time);
 					LOGGER.log(Level.INFO, "Total Time: "
 							+ totalDuration.toString().substring(2));
-					LOGGER.log(
-							Level.INFO,
+					LOGGER.log(Level.INFO,
 							"Feature Average: "
 									+ (totalDuration.toMillis() / (float) count)
 									+ " ms");
-					logTime = time;
+					LOGGER.log(Level.INFO, "Feature Insert Average: "
+							+ (insertDuration.toMillis() / (float) count)
+							+ " ms");
+					chunkInsertDuration = Duration.ofMillis(0);
+					chunkStartTime = time;
 				}
 
 			}
@@ -153,6 +190,14 @@ public class GeoPackagePerformance {
 			}
 			throw e;
 		}
+
+		Duration totalDuration = Duration.between(startTime, Instant.now());
+		LOGGER.log(Level.INFO,
+				"Final Total Time: " + totalDuration.toString().substring(2));
+		LOGGER.log(Level.INFO, "Final Feature Average: "
+				+ (totalDuration.toMillis() / (float) count) + " ms");
+		LOGGER.log(Level.INFO, "Final Feature Insert Average: "
+				+ (insertDuration.toMillis() / (float) count) + " ms");
 
 		geoPackage.close();
 
