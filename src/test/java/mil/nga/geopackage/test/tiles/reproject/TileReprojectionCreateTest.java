50b48ee44484549098b7ab97996d3cb72a5f7d6a
Brian Osborn <bosborn@gmail.com>
2021-02-01 10:31:37 -0700

    tile reprojection test setup

---
+++ src/test/java/mil/nga/geopackage/test/tiles/reproject/TileReprojectionCreateTest.java
@@ -0,0 +1,161 @@
+package mil.nga.geopackage.test.tiles.reproject;
+
+import org.junit.Test;
+
+import mil.nga.geopackage.test.CreateGeoPackageTestCase;
+
+/**
+ * Test Tile Reprojection from a created database
+ * 
+ * @author osbornb
+ */
+public class TileReprojectionCreateTest extends CreateGeoPackageTestCase {
+
+	/**
+	 * Constructor
+	 */
+	public TileReprojectionCreateTest() {
+
+	}
+
+	/**
+	 * Test reproject
+	 */
+	@Test
+	public void testReproject() {
+
+		TileReprojectionUtils.testReproject(geoPackage);
+
+	}
+
+	/**
+	 * Test reproject replacing the table
+	 */
+	@Test
+	public void testReprojectReplace() {
+
+		TileReprojectionUtils.testReprojectReplace(geoPackage);
+
+	}
+
+	/**
+	 * Test reproject of individual zoom levels
+	 */
+	@Test
+	public void testReprojectZoomLevels() {
+
+		TileReprojectionUtils.testReprojectZoomLevels(geoPackage);
+
+	}
+
+	/**
+	 * Test reproject of overwriting a zoom level
+	 */
+	@Test
+	public void testReprojectZoomOverwrite() {
+
+		TileReprojectionUtils.testReprojectZoomOverwrite(geoPackage);
+
+	}
+
+	/**
+	 * Test reproject of overwriting a table
+	 */
+	@Test
+	public void testReprojectOverwrite() {
+
+		TileReprojectionUtils.testReprojectOverwrite(geoPackage);
+
+	}
+
+	/**
+	 * Test reproject with zoom level mappings
+	 */
+	@Test
+	public void testReprojectToZoom() {
+
+		TileReprojectionUtils.testReprojectToZoom(geoPackage);
+
+	}
+
+	/**
+	 * Test reproject with zoom level matrix and tile length configurations
+	 */
+	@Test
+	public void testReprojectMatrixAndTileLengths() {
+
+		TileReprojectionUtils.testReprojectMatrixAndTileLengths(geoPackage);
+
+	}
+
+	/**
+	 * Test reproject with tile optimization
+	 */
+	@Test
+	public void testReprojectOptimize() {
+
+		TileReprojectionUtils.testReprojectOptimize(geoPackage, false);
+
+	}
+
+	/**
+	 * Test reproject with tile optimization using world bounds
+	 */
+	@Test
+	public void testReprojectOptimizeWorld() {
+
+		TileReprojectionUtils.testReprojectOptimize(geoPackage, true);
+
+	}
+
+	/**
+	 * Test reproject with web mercator tile optimization
+	 */
+	@Test
+	public void testReprojectWebMercator() {
+
+		TileReprojectionUtils.testReprojectWebMercator(geoPackage, false);
+
+	}
+
+	/**
+	 * Test reproject with web mercator tile optimization using world bounds
+	 */
+	@Test
+	public void testReprojectWebMercatorWorld() {
+
+		TileReprojectionUtils.testReprojectWebMercator(geoPackage, true);
+
+	}
+
+	/**
+	 * Test reproject with platte carre tile optimization
+	 */
+	@Test
+	public void testReprojectPlatteCarre() {
+
+		TileReprojectionUtils.testReprojectPlatteCarre(geoPackage, false);
+
+	}
+
+	/**
+	 * Test reproject with platte carre tile optimization using world bounds
+	 */
+	@Test
+	public void testReprojectPlatteCarreWorld() {
+
+		TileReprojectionUtils.testReprojectPlatteCarre(geoPackage, true);
+
+	}
+
+	/**
+	 * Test reproject cancel
+	 */
+	@Test
+	public void testReprojectCancel() {
+
+		TileReprojectionUtils.testReprojectCancel(geoPackage);
+
+	}
+
+}
---
+++ src/test/java/mil/nga/geopackage/test/tiles/reproject/TileReprojectionUtils.java
@@ -0,0 +1,132 @@
+package mil.nga.geopackage.test.tiles.reproject;
+
+import mil.nga.geopackage.GeoPackage;
+
+/**
+ * Tile Reprojection Utility test methods
+ * 
+ * @author osbornb
+ */
+public class TileReprojectionUtils {
+
+	/**
+	 * Test read
+	 * 
+	 * @param geoPackage
+	 *            GeoPackage
+	 */
+	public static void testReproject(GeoPackage geoPackage) {
+		// TODO
+	}
+
+	/**
+	 * Test reproject replacing the table
+	 * 
+	 * @param geoPackage
+	 *            GeoPackage
+	 */
+	public static void testReprojectReplace(GeoPackage geoPackage) {
+		// TODO
+	}
+
+	/**
+	 * Test reproject of individual zoom levels
+	 * 
+	 * @param geoPackage
+	 *            GeoPackage
+	 */
+	public static void testReprojectZoomLevels(GeoPackage geoPackage) {
+		// TODO
+	}
+
+	/**
+	 * Test reproject of overwriting a zoom level
+	 * 
+	 * @param geoPackage
+	 *            GeoPackage
+	 */
+	public static void testReprojectZoomOverwrite(GeoPackage geoPackage) {
+		// TODO
+	}
+
+	/**
+	 * Test reproject of overwriting a table
+	 * 
+	 * @param geoPackage
+	 *            GeoPackage
+	 */
+	public static void testReprojectOverwrite(GeoPackage geoPackage) {
+		// TODO
+	}
+
+	/**
+	 * Test reproject with zoom level mappings
+	 * 
+	 * @param geoPackage
+	 *            GeoPackage
+	 */
+	public static void testReprojectToZoom(GeoPackage geoPackage) {
+		// TODO
+	}
+
+	/**
+	 * Test reproject with zoom level matrix and tile length configurations
+	 * 
+	 * @param geoPackage
+	 *            GeoPackage
+	 */
+	public static void testReprojectMatrixAndTileLengths(
+			GeoPackage geoPackage) {
+		// TODO
+	}
+
+	/**
+	 * Test reproject with tile optimization
+	 * 
+	 * @param geoPackage
+	 *            GeoPackage
+	 * @param world
+	 *            world bounds
+	 */
+	public static void testReprojectOptimize(GeoPackage geoPackage,
+			boolean world) {
+		// TODO
+	}
+
+	/**
+	 * Test reproject with web mercator tile optimization
+	 * 
+	 * @param geoPackage
+	 *            GeoPackage
+	 * @param world
+	 *            world bounds
+	 */
+	public static void testReprojectWebMercator(GeoPackage geoPackage,
+			boolean world) {
+		// TODO
+	}
+
+	/**
+	 * Test reproject with platte carre tile optimization
+	 * 
+	 * @param geoPackage
+	 *            GeoPackage
+	 * @param world
+	 *            world bounds
+	 */
+	public static void testReprojectPlatteCarre(GeoPackage geoPackage,
+			boolean world) {
+		// TODO
+	}
+
+	/**
+	 * Test reproject cancel
+	 * 
+	 * @param geoPackage
+	 *            GeoPackage
+	 */
+	public static void testReprojectCancel(GeoPackage geoPackage) {
+		// TODO
+	}
+
+}
